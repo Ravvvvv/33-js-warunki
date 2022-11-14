a36c632332dd708b3015b3aa900af17e65ad2ae1
rafal a <rafalandrzejczuk1@gmial.com>
2022-11-14 15:40:26 +0100

    zadanie

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let cyfra = 12;
+
+if (cyfra === 12) {
+    console.log("Podana liczba jest większa od 10")
+}
+else if (cyfra === 13){
+
+    console.log("Podana liczba jest wieksza od 10" )
+    }
+else { 
+console.log("zaden z powyszych ")
+
+
+}
