10f147b4348eacef0f832bc543ac98da700dfa08
rafal A <rafalandrzejczuk1@gmail.com>
2022-11-17 14:06:59 +0100

    zamian2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let cyfra = 12;
 
-if (cyfra === 12) {
+if (cyfra > 10) {
     console.log("Podana liczba jest większa od 10")
 }
-else if (cyfra === 13){
+else if (cyfra === 12){
 
-    console.log("Podana liczba jest wieksza od 10" )
+    console.log("Jeśli liczba w zmiennej cyfra jest równa 12, warunek zostanie spełniony" )
     }
 else { 
 console.log("zaden z powyszych ")
